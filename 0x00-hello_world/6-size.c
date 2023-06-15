da897bc7132ef2ff9aa2b5ac0559c1e7cacea973
Mashumi <shum26@live.com>
2023-06-15 15:21:28 -0700

    Added 6-sizeof.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various data-types.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c;
+int i, li, lli;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
+printf("Size of a int: %d byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
