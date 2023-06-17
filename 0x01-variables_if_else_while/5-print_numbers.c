9e9261be9935819a3aacf6ea25fbafaef42c4c62
Mashumi <shum26@live.com>
2023-06-17 11:17:51 -0700

    Added 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Prints numbers 0-9'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+
+return (0);
+}
+
