d665d208d24bd68511a71a86ab19080654724eca
Mashumi <shum26@live.com>
2023-06-20 12:36:47 -0700

    Troubleshooting

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,24 +6,5 @@
  */
 int print_sign(int n)
 {
-int retval, c;
-c = n;
-
-if (c > 0 && c != 0)
-{
-_putchar('+');
-retval = 1;
-}
-else if (c < 0 && c != 0)
-{
-_putchar('-');
-retval = -1;
-}
-else
-{
-_putchar('0');
-retval = 0;
-}
-
 return (retval);
 }
