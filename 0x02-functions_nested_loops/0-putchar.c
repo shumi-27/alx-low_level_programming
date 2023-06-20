589611633df72aafd363d0bd94cd9df2bc65e31a
Mashumi <shum26@live.com>
2023-06-20 05:59:00 -0700

    Added 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main - Print _puchar
+ * Return: Always 0.
+ */
+void main(void)
+{
+	print_putchar();
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+void print_putchar(void)
+{
+	char pr[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8 ; i++)
+	{
+		_putchar(pr[i]);
+	}
+
+	_putchar('\n');
+}
