7d39d682dfd0855990b470a8aca43e4f9158ac50
Mashumi <shum26@live.com>
2023-06-20 06:54:39 -0700

    Changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,3 @@
 int _putchar(int);
 
-void print_putchar(void)
-{
-	char pr[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8 ; i++)
-	{
-		_putchar(pr[i]);
-	}
-
-	_putchar('\n');
-}
-
 void print_alphabet(void);
