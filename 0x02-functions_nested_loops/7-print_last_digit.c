68c8a255c828b453156267a5cf35c35f7ad3ee15
Mashumi <shum26@live.com>
2023-06-20 14:29:52 -0700

    Added 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * print_last_digit - computes the last digit of a number
+  * @retval : Accepts number.
+  * Return: Last digit of a number.
+  */
+int print_last_digit(int retval)
+{
+	return (retval % 10);
+}
