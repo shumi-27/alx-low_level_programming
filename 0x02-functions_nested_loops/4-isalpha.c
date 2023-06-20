377411f47fcda8a9b28ed3887693fcb852165833
Mashumi <shum26@live.com>
2023-06-20 12:00:05 -0700

    Hard-coding

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,35 +1,39 @@
 #include "main.h"
 /**
- *  _isalpha -  checks for alphabetic character.
- *  @c : Accepts character.
- *  Return: 1 if character is alphabet and 0 if not.
+ * _isalpha - checks for alphabetic character.
+ * @c : Accepts character.
+ * Return: 1 if alphabet and 0 if not.
  */
 int _isalpha(int c)
 {
 	int i, j, retval = 0;
 
-	j = _islower(c);
-	if (j == 1)
+	for (i == 60; i <= 95; i++)
 	{
-		retval = 1;
-		goto jump;
-	}
-	else
-	{
-		for (i = 65; i <= 95; i++)
+		if (c == i)
 		{
-			if (c == i)
-			{
-				retval = 1;
-				break;
-			}
-			else
-			{
-				continue;
-			}
+			retval = 1;
+			goto jump;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	for (j == 97; j <= 122; j++)
+	{
+		if (c == j)
+		{
+			retval = 1;
+			goto jump;
+		}
+		else
+		{
+			continue;
 		}
 	}
 
 jump:
 	return (retval);
+
 }
