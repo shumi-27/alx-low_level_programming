9362022124d4408e8df7b97a7c31cd7ec0537b02
Mashumi <shum26@live.com>
2023-06-20 16:18:04 -0700

    Fixes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,14 @@
   */
 void times_table(void)
 {
-	int u, i;
+	int u, i, A;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (u = 0; u < 10; u++)
 		{
-			_putchar((u * i));
+			A = u * i;
+			_putchar(A + '0');
 			if (u != 9)
 			{
 				if ((u * (i + 1)) > 10 || (u * (i + 1)) == 10)
