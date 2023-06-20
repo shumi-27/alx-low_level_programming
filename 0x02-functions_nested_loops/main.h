102c5e7c6fc1a6b58f8939297a02049a043022dd
Mashumi <shum26@live.com>
2023-06-20 07:58:48 -0700

    Header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,2 @@
 void _putchar(int);
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-
-	_putchar('\n');
-
-	return;
-}
+void print_alphabet(void);
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,12 @@
+{
+        int i;
+
+        for (i = 'a'; i <= 'z'; i++)
+        {
+                _putchar(i);
+        }
+
+        _putchar('\n');
+
+        return;
+}
