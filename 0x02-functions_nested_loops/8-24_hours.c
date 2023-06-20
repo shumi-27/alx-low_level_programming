99eb0145366cd8e6030fdd930164d870c0180842
Mashumi <shum26@live.com>
2023-06-20 15:11:00 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * jack_bauer - computes the absolute value of an integer
+  * Return: Always 0;
+  */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 23; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_puthchar('i');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+
+	}
+
+}
