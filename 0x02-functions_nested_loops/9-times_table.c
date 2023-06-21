80a18a40a07c5450ff9efe8414f4906297dbbd5a
Mashumi <shum26@live.com>
2023-06-20 18:26:29 -0700

    Fixed void 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,42 +1,42 @@
 #include "main.h"
 /**
-  * times_table - Creates nine-times table.
+  * times_table - computes 9 x table.
   * Return: Always 0.
   */
 void times_table(void)
 {
-	int u, i, A;
+int i, j, cnt, u, x;
 
-	for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i ++)
+{
+	for (j = 0; j < 10; j ++)
 	{
-		for (u = 0; u < 10; u++)
+		cnt = i * j;
+		if (cnt >= 10)
 		{
-			A = u * i;
-			if (A < 10)
-			{
-				_putchar(A + '0');
-			}
-			if (u != 9)
-			{
-				if ((u * (i + 1)) > 10 || (u * (i + 1)) == 10)
-				{
-					_putchar((A / 10) + '0');
-					_putchar((A % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-
-			}
+		u = cnt/10;
+		x = cnt % 10;
+		_putchar(44);
+		_putchar(32);
+		_putchar(u + '0');
+		_putchar(x + '0');
 
 		}
-	_putchar('\n');
+		else
+		{
+		if (cnt != 0)
+		{
+		_putchar(44);
+		_putchar(32);
+		_putchar(32);
+		_putchar(cnt + '0');
+		}
+		_putchar('i');
+		}
+
 	}
+}
+
+_putchar('\n');
 
 }
