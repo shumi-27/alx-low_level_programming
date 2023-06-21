cda6bf6667f714d430c3fa1ca011ed5c97c89c93
Mashumi <shum26@live.com>
2023-06-20 17:07:04 -0700

    Corrections

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i => 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if(i != 98)
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d");
+			printf("%d", i);
 			if(i != 98)
 			{
 				printf(", ");
