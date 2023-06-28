d90664338e2495c100a79f1654c6d80add658ca9
Mashumi <shum26@live.com>
2023-06-28 14:26:03 -0700

    Made corrections

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,19 +8,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	char *s1, *s2;
+	char *s1;
 
 	s1 = dest;
-	s2 = src;
 	while (*s1 != '\0')
 	{
 		s1++;
 	}
-	for (i = 0; i < n; i++)
+	while (*src && n > 1)
 	{
-		*s1 = *(s2 + i);
+		*s1 = *src;
+		n--;
 		s1++;
+		src++;
 	}
 	*s1 = '\0';
 	return (dest);
