00732dac8abaa4860ff38adb4c335b4939a6cf29
Mashumi <shum26@live.com>
2023-06-29 15:06:36 -0700

    Attempt to fix 6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,31 +6,24 @@
  */
 char *cap_string(char *str)
 {
-	int i, flag;
+	int i;
 	char *str2 = str;
 	char spr[] = {' ', '\t', '\n', ',', ';', '.', '"', '(', ')', '{', '}'};
 
 	while (*str2 != '\0')
 	{
-		flag = 0;
-		for (i = 0; i < 11; i++)
+		if (*str2 >= 'a' && *str2 <= 'z')
 		{
-			if (*str2 == spr[i])
+			for (i = 0; i < 12; i++)
 			{
-				flag = 1;
-				break;
-			}	
-		}
-
-		if (flag == 1)
-		{
-			str2++;
-			if (*str2 >= 'a' && *str2 <= 'z')
-			{
-				*str2 = *str2 -32;
+				if (*(str2 - 1) == spr[i])
+				{
+					*str2 = *str2 - 32;
+					break;
+				}
 			}
 		}
-		str2++;
+	str2++;
 	}
 	return (str);
 }
