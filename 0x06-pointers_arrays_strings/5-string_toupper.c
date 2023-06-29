4be4a69f8522f5255fd73826671366532cf5bb1f
Mashumi <shum26@live.com>
2023-06-29 10:16:35 -0700

    Betty corrections

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * *string_toupper -  changes all lowercase letters of a string to uppercase.
- * @Str - String.
+ * @Str : String.
  * Return: Always 0.
  */
 char *string_toupper(char *Str)
