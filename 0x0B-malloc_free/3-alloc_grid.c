fa2108e3aa70c05a0a17fad593cf8abb3ef5a1b4
Mashumi <shum26@live.com>
2023-07-11 03:35:34 -0700

    Corrections

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,6 +22,16 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i <  height; i++)
 	{
 		grid[i] = malloc(width);
+		if (grid[i] == NULL)
+		{
+			for(j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (0);
+		}
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
