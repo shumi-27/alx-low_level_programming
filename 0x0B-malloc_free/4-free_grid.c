4ae60f99df39b205f05c1b0d835c587924386d0d
Mashumi <shum26@live.com>
2023-07-11 13:55:58 -0700

    Betty corrections to 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid function
+ * free_grid - frees a 2 dimensional grid created by alloc_grid function
  * @grid :  grid.
  * @height : rows.
  * Return: Always 0.
