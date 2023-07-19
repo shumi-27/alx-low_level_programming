d06a29a8f6ca46ab97a5703e5c0eee0965979aa4
Mashumi <shum26@live.com>
2023-07-19 03:23:41 -0700

    Corrections

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int j, flag;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 	for (j = 0; j < size; j++)
 	{
