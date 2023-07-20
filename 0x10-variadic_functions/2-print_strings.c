910b29f10c3986351f3cb51f3bed03720c73a2fb
Mashumi <shum26@live.com>
2023-07-20 06:07:21 -0700

    Corrections

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,8 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
-	if (n == 0)
-		return;
 	va_start(strings, n);
 	if (separator == NULL)
 		separator = "";
